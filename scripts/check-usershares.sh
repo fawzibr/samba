2b9930c4289de56754cd9d178bba21cd376803a9
root <root@ubuntu-test.spectron-msim.com>
2024-10-25 15:02:05 +0000

    Fixed bug,added some log information when adding/deleting shares   scripts/check-usershares.sh

--- scripts/check-usershares.sh
+++ scripts/check-usershares.sh
@@ -1,5 +1,69 @@
 #!/bin/sh
 
+####################################
+# FUNCTIONS
+####################################
+
+add_share(){
+	SN=$1 # SHARE NAME
+	SD=$2 # SHARE DIR
+	SC=$3 # SHARE COMMENT
+	SP=$4 # SHARE PERMISSIONS
+	SG=$5 # SHARE GUEST OK
+	# validate arguments
+	if [ -z "$SN" ]; then
+		echo "  ERROR: Share name is empty"
+		return 1
+	elif [ -z "$SD" ]; then
+		echo "  ERROR: Path variable is empty"
+		return 1
+	elif [ ! -d "$SD" ]; then
+		echo "  ERROR: Path is missing or not a directory"
+		return 1
+	fi
+	# config permissions and guest
+	if [ -z "$SP" ] && [ -z "$SG" ] ; then # not set
+		NET_OPTS=""
+	elif [ ! -z "$SP" ] && [ -z "$SG" ] ; then # acl set, guest not set
+		NET_OPTS="$SP"
+	elif [ -z "$SP" ] && [ ! -z "$SG" ] ; then # acl not set, guest set
+		NET_OPTS="Everyone:R guest_ok=$SG"
+	else # acl/guest set
+		NET_OPTS="$SP guest_ok=$SG"
+	fi
+	# start share
+	echo ">> USERSHARE: Add/Modify [$SN]"
+	echo ">> USERSHARE: net usershare add \"$SN\" \"$SD\" \"${SC:-$SC share}\" $NET_OPTS"
+	if net usershare add "$SN" "$SD" "${SC:-$SC share}" $NET_OPTS; then
+		echo "     SUCCESS"
+                net usershare info --long "$SN" | while IFS= read -r LINE ; do
+  			echo "     $LINE"
+                done
+		return 0
+	else
+		echo "     ERROR"
+		return 1
+	fi
+}
+
+delete_share(){
+	SN="$1"
+	#
+	echo ">> USERSHARE: Add/Modify [$SN]"
+	echo ">> USERSHARE: net usershare delete \"$SN\""
+	if net usershare delete "$SN"; then 
+		echo "  SUCCESS"
+		return 0
+	else
+		echo "  ERROR"
+		return 1
+	fi
+}
+
+####################################
+# MAIN
+####################################
+
 USERSHARE_DIR=$1
 CHECKSUM_PREV=/tmp/checksum.prev
 CHECKSUM_CURR=/tmp/checksum.curr
@@ -43,37 +107,17 @@ for FILE_NAME in $USERSHARE_FILES; do
 	PREV_MD5=$(cat $CHECKSUM_PREV | grep "$FILE_NAME" | cut -f 1 -d " ")
 	CURR_MD5=$(cat $CHECKSUM_CURR | grep "$FILE_NAME" | cut -f 1 -d " ")
 	# process usershare
-	NET_ARGS=""
-	if [ "$PREV_MD5" != "" ] && [ "$CURR_MD5" = "" ]; then # delete
-		echo ">> USERSHARE: Delete [$SHARE_NAME]"
-		NET_ARGS="delete \"$SHARE_NAME\""
-	elif [ "$PREV_MD5" != "$CURR_MD5" ]; then # added or modified
-		echo ">> USERSHARE: Add/Modify [$SHARE_NAME]"
+	RESULT=""
+	if [ "$PREV_MD5" != "" ] && [ "$CURR_MD5" = "" ]; then # share file deleted
+		# delete the share from samba
+		delete_share "$SHARE_NAME"
+	elif [ "$PREV_MD5" != "$CURR_MD5" ]; then # share file added or changed
 		# get variables from usershare file
 		SHARE_PATH=$(cat $FILE_NAME | grep -i "path\s*=" $FILE_NAME | cut -f 2 -d "="  | xargs)
 		SHARE_COMMENT=$(cat $FILE_NAME | grep -i "comment\s*=" $FILE_NAME | cut -f 2 -d "="  | xargs)
 		SHARE_ACL=$(cat $FILE_NAME | grep -i "acl\s*=" $FILE_NAME | cut -f 2 -d "="  | xargs)
 		SHARE_GUEST=$(cat $FILE_NAME | grep -i "guest_ok\s*=" $FILE_NAME | cut -f 2 -d "="  | xargs)
-		# create args
-		if [ -z "$SHARE_PATH" ]; then
-			echo "  ERROR: Path variable not set in $FILE_NAME"
-		elif [ -z "$SHARE_ACL" ] && [ -z "$SHARE_GUEST" ] ; then # acl/guest not set
-			NET_ARGS="add \"$SHARE_NAME\" \"$SHARE_PATH\" \"${SHARE_COMMENT:-$SHARE_NAME share}\""
-		elif [ ! -z "$SHARE_ACL" ] && [ -z "$SHARE_GUEST" ] ; then # acl set, guest not set
-			NET_ARGS="add \"$SHARE_NAME\" \"$SHARE_PATH\" \"${SHARE_COMMENT:-$SHARE_NAME share}\" \"$SHARE_ACL\""
-		elif [ -z "$SHARE_ACL" ] && [ ! -z "$SHARE_GUEST" ] ; then # acl not set, guest set
-			NET_ARGS="add \"$SHARE_NAME\" \"$SHARE_PATH\" \"${SHARE_COMMENT:-$SHARE_NAME share}\" \"Everyone:R\" \"guest_ok=$SHARE_GUEST\""
-		else # acl/guest set
-			NET_ARGS="add \"$SHARE_NAME\" \"$SHARE_PATH\" \"${SHARE_COMMENT:-$SHARE_NAME share}\" \"$SHARE_ACL\" \"guest_ok=$SHARE_GUEST\""
-		fi
-		# execute command
-		if [ ! -z "$NET_ARGS" ]; then
-			echo ">> USERSHARE: net usershare $NET_ARGS"
-			if ! net usershare $NET_ARGS; then
-				echo "  ERROR"
-			else
-				echo "  SUCCESS"
-			fi
-		fi
+		# add share to samba
+		add_share "$SHARE_NAME" "$SHARE_PATH" "$SHARE_COMMENT" "$SHARE_ACL" "$SHARE_GUEST"
 	fi
 done
